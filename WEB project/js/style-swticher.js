ed1d9ffb2180adba8c9c56c3f36f10b3ec1fae54
abhigirme <119883561+abhigirme@users.noreply.github.com>
2022-12-05 22:43:26 +0530

    Add files via upload

---
+++ WEB project/js/style-swticher.js
@@ -0,0 +1,6 @@
+// /====================toggle style swticher=======================/
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", ()=> {
+    document.querySelector(".style-swticher").classList.toggle(".open");
+})
+2:42:57,
