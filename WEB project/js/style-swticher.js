7520ae1301471ef7061a6a68553bdf2402aa6eb2
abhigirme <119883561+abhigirme@users.noreply.github.com>
2022-12-08 18:39:54 +0530

    Delete WEB project directory

--- WEB project/js/style-swticher.js
+++
@@ -1,6 +0,0 @@
-// /====================toggle style swticher=======================/
-const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
-styleSwitcherToggle.addEventListener("click", ()=> {
-    document.querySelector(".style-swticher").classList.toggle(".open");
-})
-2:42:57,
